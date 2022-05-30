c79afaa69659ea0b48ac1cc1c2a32052c451508f
Allen <44152004+allenfc0@users.noreply.github.com>
2022-05-29 20:37:18 -0700

    Delete arrayFront9.java

--- arrayFront9.java
+++
@@ -1,9 +0,0 @@
-public boolean arrayFront9(int[] nums) {
-  //need a max number to loop for conditional
-  int len = Math.min(nums.length, 4);
-  
-  for(int i = 0; i < len; i++) {
-    if(nums[i] == 9) return true;
-  }
-  return false;
-}
