c4fc3b5095287773014771ef31e84c7c5f81b01d
Allen Farias <allenfarias29@gmail.com>
2023-07-31 16:59:48 -0700

    makeEnds completed

---
+++ Array1/makeEnds.java
@@ -0,0 +1,12 @@
+/*
+ * Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
+ */
+
+public int[] makeEnds(int[] nums) {
+  int [] arr = new int[2];
+  
+  arr[0] = nums[0];
+  arr[1] = nums[nums.length - 1];
+  
+  return arr;
+}
