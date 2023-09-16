b6a6c3ab27ba19628eaf15f1ed0e5258d88f3c95
Allen Farias <allenfarias29@gmail.com>
2023-09-15 23:23:05 -0700

    plusTwo completed

---
+++ Array1/plusTwo.java
@@ -0,0 +1,11 @@
+
+/*
+ * Given 2 int arrays, each length 2, return a new array length 
+ * 4 containing all their elements.
+ */
+public int[] plusTwo(int[] a, int[] b) {
+  int[] c = {a[0], a[1], b[0], b[1]};
+  
+  return c;
+  
+}
