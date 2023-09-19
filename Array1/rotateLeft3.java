4fb831d98127072be857ad3ca7f443fd8cef4bff
Allen Farias <allenfarias29@gmail.com>
2023-09-19 12:07:40 -0700

    rotateLeft3 completed

---
+++ Array1/rotateLeft3.java
@@ -0,0 +1,10 @@
+
+/*
+ * Given an array of ints length 3, return an array with the 
+ * elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+ */
+public int[] rotateLeft3(int[] nums) {
+  int[] arr = {nums[1], nums[2], nums[0]};
+  
+  return arr;
+}
