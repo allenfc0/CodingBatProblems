522b3439be1fa4c96883a546c9716531934d0d69
Allen Farias <allenfarias29@gmail.com>
2023-11-05 21:00:56 -0700

    greenTicket completed

--- Logic-1/greenTicket.java
+++ Logic-1/greenTicket.java
@@ -1,11 +1,16 @@
+
+/*
+ * You have a green lottery ticket, with ints a, b, and c on it. 
+ * If the numbers are all different from each other, the result 
+ * is 0. If all of the numbers are the same, the result is 20. 
+ * If two of the numbers are the same, the result is 10.
+ */
 public int greenTicket(int a, int b, int c) {
-  int count = 0;
+  if(a == b && b == c) {
+    return 20;
+  } else if(a != b && b != c && a != c) {
+    return 0;
+  }
   
-  if(a == b) count++;
-  if(b == c) count++;
-  if(c == a) count++;
-  
-  if(count == 3) return 20;
-  if(count == 2) return 10;
-  return 0;
+  return 10;
 }
